6998f677ad588b105ba66408e6ae5fb8e6b8e814
Kalene <kalenenc@gmail.com>
2017-02-13 22:29:51 -0500

    edited member controller to include subscription information

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,9 +2,8 @@ class MembersController < ApplicationController
 
   # GET /members
   def index
-    @members = Member.all
-
-    render json: @members
+  	@members = Member.all
+    render json: @members, :include => [:subscription]
   end
 
 end
