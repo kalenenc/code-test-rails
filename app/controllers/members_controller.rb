a7baf8546e0610fdd4342d067ccfb399a2ba39e8
Kalene <kalenenc@gmail.com>
2017-02-14 00:02:44 -0500

    tweaked members controller

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,8 +2,10 @@ class MembersController < ApplicationController
 
   # GET /members
   def index
+  
   	@members = Member.all
     render json: @members, :include => [:subscription]
+  	
   end
 
 end
